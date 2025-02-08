b20b4af2302529af65646d4e5bbb1d8f4ca46e7a
TheRadhe <rajveer202415@gmail.com>
2025-02-08 15:05:01 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -136,7 +136,9 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+        await asyncio.sleep(2)
+usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt}\n\n{msg}"
